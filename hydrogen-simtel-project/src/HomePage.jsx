e019e293ffbdc42182a48bfa365b73bf43833eba
unknown <himanshujain.adt@gmail.com>
2024-12-10 15:00:24 +0530

    HomePageChanged

--- hydrogen-simtel-project/src/HomePage.jsx
+++ hydrogen-simtel-project/src/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
                 <img src={pixelcutImage} />
                 <div id="rel">
                     <h2 id="sim-title">HYDROGEN SIMTEL LEARNING</h2>
-                    <Button variant="contained" color="primary">Enter</Button>
+                   
                 </div>
 
 
